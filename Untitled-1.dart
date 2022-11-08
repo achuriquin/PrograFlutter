8ad27562b56ec207b0888dc2fe4282b9547e6ece
achuriquin <dfghdfgdfg90@gmail.com>
2022-11-08 15:34:14 -0500

    primer dart

---
+++ Untitled-1.dart
@@ -0,0 +1,185 @@
+void main() {
+  int n1=10;
+  int n2=20;
+  int n3=30;
+  int n4=40;
+  int n5=50;
+  int n6=60;
+  int n7=70;
+  int n8=80;
+  int n9=90;
+  if(n1>n2){
+    if(n1>n3){
+      if(n1>n4){
+        if(n1>n5){
+          if(n1>n6){
+            if(n1>n7){
+              if(n1>n8){
+                if(n1>n9){
+                  print(n1);
+                }else{
+                  if(n2>n3){
+                    if(n2>n4){
+                      if(n2>n5){
+                        if(n2>n6){
+                          if(n2>n7){
+                            if(n2>n8){
+                              if(n2>n9){
+                                if(n2>n1){
+                                  print(n2);
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }else{
+                if(n3>n4){
+                  if(n3>n5){
+                    if(n3>n6){
+                      if(n3>n7){
+                        if(n3>n8){
+                          if(n3>n9){
+                            if(n3>n2){
+                              if(n3>n1){
+                                print(n3);
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }else{
+              if(n4>n5){
+                if(n4>n6){
+                  if(n4>n7){
+                    if(n4>n8){
+                      if(n4>n9){
+                          if(n4>n3){
+                            if(n4>n2){
+                              if(n4>n1){
+                                print(n4);
+                              }
+                            }
+                          }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }else{
+            if(n5>n6){
+              if(n5>n7){
+                if(n5>n8){
+                  if(n5>n9){
+                    if(n5>n4){
+                      if(n5>n3){
+                        if(n5>n2){
+                          if(n5>n1){
+                            print(n5);
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }else{
+          if(n6>n7){
+            if(n6>n8){
+              if(n6>n9){
+                if(n6>n5){
+                  if(n6>n4){
+                    if(n6>n3){
+                      if(n6>n2){
+                        if(n6>n1){
+                          print(n6);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }else{
+        if(n7>n8){
+          if(n7>n9){
+            if(n7>n6){
+              if(n7>n5){
+                if(n7>n4){
+                  if(n7>n3){
+                    if(n7>n2){
+                      if(n7>n1){
+                        print(n7);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            
+          }
+        }
+      }
+    }else{
+      if(n8>n9){
+        if(n8>n7){
+          if(n8>n6){
+            if(n8>n5){
+              if(n8>n4){
+                if(n8>n3){
+                  if(n8>n2){
+                    if(n8>n1){
+                      print(n8);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }else{
+    if(n9>n8){
+      if(n9>n7){
+        if(n9>n6){
+          if(n9>n5){
+            if(n9>n4){
+              if(n9>n3){
+                if(n9>n2){
+                  if(n9>n1){
+                      print(n9);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  else{
+    print("n8");
+  }
+  }
+  //for (int i = 0; i < 5; i++) {
+    //print('Hola mundo');
+    //String nOmbre="Andy";
+   // int edad=29;
+   // double sueldo=10.5;
+   // print(nOmbre);
+   // print(edad);
+   // print(sueldo);
+ // } 
+}
